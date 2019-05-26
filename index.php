c82c409a4098088187b777bf7c3dc7db28ee2f06
taira <xxnagomixx777@gmail.com>
2019-05-26 20:20:42 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="UTF-8">
+	<title>Users</title>
+</head>
+<body>
+	<form action="users.php" name="" method="post" enctype="application/x-www-form-urlencoded">
+		<input type="text" name="name">
+		<input type="text" name="email">
+		<input type="password" name="password">
+		<input type="submit" name="" value="送信">
+
+</form>
+
+</body>
+</html>
