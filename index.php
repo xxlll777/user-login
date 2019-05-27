c61d3cd0d2ea43dc2bf3db68a95fbedd89aec477
taira <xxnagomixx777@gmail.com>
2019-05-27 21:55:09 +0900

    scss作成

--- index.php
+++ index.php
@@ -1,16 +1,39 @@
+<?php
+$root_url = get_stylesheet_directory_uri() ;
+$css_url = $root_url . "/css";
+$img_url = $root_url . "/images";
+$js_url  = $root_url . "/js";
+?>
+
 <!DOCTYPE html>
 <html lang="ja">
 <head>
 	<meta charset="UTF-8">
 	<title>Users</title>
+	<link rel="stylesheet" href="<?php echo $css_url; ?>/style.css">
 </head>
 <body>
-	<form action="users.php" name="" method="post" enctype="application/x-www-form-urlencoded">
-		<input type="text" name="name">
-		<input type="text" name="email">
-		<input type="password" name="password">
-		<input type="submit" name="" value="送信">
 
+<form action="users.php" name="" method="post" enctype="application/x-www-form-urlencoded">
+	<div class="registration">
+		<div class="reg__ttl">
+			ユーザー登録
+		</div>
+		<div class="reg__name">
+			<p></p>
+			<input type="text" name="name" placeholder="Name">
+		</div>
+		<div class="reg__email">
+			<input type="text" name="email" placeholder="Email">
+		</div>
+		<div class="reg__pass">
+			<input type="password" name="password" placeholder="Password">
+		</div>
+		<div class="reg__btn">
+			<input type="submit" name="" value="登録">
+		</div>
+
+	</div>
 </form>
 
 </body>
